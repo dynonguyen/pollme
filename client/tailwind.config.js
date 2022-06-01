020373c55474fb1b5a9dbe74c6d7cb1182622088
Tuan Nguyen <tuannguyentn2504@gmail.com>
2022-06-01 22:45:15 +0700

    Client: Theme config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,8 +3,30 @@ module.exports = {
 		'./pages/**/*.{js,ts,jsx,tsx}',
 		'./components/**/*.{js,ts,jsx,tsx}',
 	],
+	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				// light mode
+				primary: '#2d8bba',
+				secondary: '#41b8d5',
+				accent: '#f260cd',
+				'text-title': '#0f172a',
+				'text-primary': '#334155',
+
+				// dark mode
+				'dark-bg': '#0f172a',
+				'dark-primary': '#287da7',
+				'dark-secondary': '#2dadcc',
+				'dark-accent': '#ef40c3',
+				'dark-title': '#e2e8f0',
+				'dark-primary': '#94a3b8',
+			},
+
+			fontFamily: {
+				inter: ['Inter', 'Roboto', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
 };
