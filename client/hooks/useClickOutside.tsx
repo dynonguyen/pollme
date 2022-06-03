dcd4c9fe2b9da8ab8830b48edbfb34e11793adda
Tuan Nguyen <tuannguyentn2504@gmail.com>
2022-06-03 21:21:55 +0700

    Client/hook: useClickOutSide hook

---
+++ client/hooks/useClickOutside.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+
+export default function useOnClickOutside(
+	ref: React.MutableRefObject<any>,
+	handler: Function,
+) {
+	useEffect(() => {
+		const listener = (event: Event) => {
+			// Do nothing if clicking ref's element or descendent elements
+			if (!ref.current || ref.current.contains(event.target)) {
+				return;
+			}
+			handler(event);
+		};
+		document.addEventListener('mousedown', listener);
+		document.addEventListener('touchstart', listener);
+		return () => {
+			document.removeEventListener('mousedown', listener);
+			document.removeEventListener('touchstart', listener);
+		};
+	}, [ref, handler]);
+}
