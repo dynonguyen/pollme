e1ab6043e29c134d9513b52101d7665c45c53e60
Tuan Nguyen <tuannguyentn2504@gmail.com>
2022-06-11 19:10:24 +0700

    Client/Component core: Checkbox Switch

---
+++ client/components/core/CheckboxSwitch.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+
+interface CheckboxSwitchProps {
+	rootClassName?: string;
+	label?: string | React.ReactNode;
+	inputProps?: React.HTMLProps<HTMLInputElement>;
+	size?: 'small' | 'medium';
+}
+
+export default function CheckboxSwitch(
+	props: CheckboxSwitchProps,
+): JSX.Element {
+	const { inputProps = {}, label, rootClassName = '', size = 'small' } = props;
+	const [checked, setChecked] = useState(false);
+
+	const onChecked = () => {
+		setChecked(!checked);
+	};
+
+	const switchBg = checked
+		? 'bg-primary dark:bg-d_primary'
+		: 'bg-gray-300 dark:bg-gray-500';
+	const switchPosition = checked ? 'right-[2px]' : 'left-[2px]';
+	const switchSize = size === 'small' ? 'w-12 h-6' : 'w-14 h-7';
+	const switchBtnSize = size === 'small' ? 'w-5 h-5' : 'w-6 h-6';
+
+	return (
+		<div
+			className={`flex flex-wrap gap-2 items-center justify-between ${rootClassName}`}
+		>
+			{label && <label onClick={onChecked}>{label}</label>}
+			<input
+				type='checkbox'
+				className='hidden'
+				checked={checked}
+				{...inputProps}
+			/>
+			<div className={`${switchSize} ${switchBg} rounded-full relative`}>
+				<div
+					className={`absolute bg-white top-[2px] rounded-full cursor-pointer ${switchBtnSize} ${switchPosition}`}
+					onClick={onChecked}
+				></div>
+			</div>
+		</div>
+	);
+}
